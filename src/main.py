a33abaaba866c188b351eb9e7b38d163a6fb5437
Joohong Kim <jhkimsharp@gmail.com>
2010-11-25 02:36:48 +0900

    webapp hello world version

--- src/main.py
+++ src/main.py
@@ -1,5 +1,18 @@
 #!/usr/bin/env python
 
-print
-print 'Hello, World!'
+import wsgiref.handlers
 
+from google.appengine.ext import webapp
+
+class MainHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hello, World!!')
+
+def main():
+    app = webapp.WSGIApplication([
+            (r'.*', MainHandler)
+            ], debug=True)
+    wsgiref.handlers.CGIHandler().run(app)
+
+if __name__ == '__main__':
+    main()
